8a69ea437c09762d644a2f3b8ea1cf083ed486bf
Larry Mateo Ramirez C <larry.ramirez@correounivalle.edu.co>
2023-03-21 17:29:35 -0500

    Initial commit

---
+++ src/main/java/co/com/sofka/pokemoncenterpc/PokemonCenterPcApplication.java
@@ -0,0 +1,13 @@
+package co.com.sofka.pokemoncenterpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonCenterPcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokemonCenterPcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/com/sofka/pokemoncenterpc/PokemonCenterPcApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.sofka.pokemoncenterpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonCenterPcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
